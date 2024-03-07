75cdf805da4ba3c7d493489fbfabd5d24a7a5ffc
Tanner Reits <47483144+tanner-reits@users.noreply.github.com>
2024-03-07 10:41:09 -0500

    feat(build): add esbuild based build process (#5)
    * copy POC work
    
    * resolve todo comments, hard dep on dev build (temporary)
    
    * misc cleanup
    
    * build using esbuild
    
    * remove modules that are already external when targeting node

---
+++ build/build.ts
@@ -0,0 +1,27 @@
+import * as esbuild from 'esbuild';
+
+async function build() {
+  const baseOptions: esbuild.BuildOptions = {
+    bundle: true,
+    entryPoints: ['src/index.ts'],
+    external: ['@playwright/test', '@stencil/core'],
+    platform: 'node',
+    sourcemap: 'linked',
+    target: 'node16',
+  };
+
+  await Promise.all([
+    esbuild.build({
+      ...baseOptions,
+      outfile: 'dist/index.js',
+      format: 'esm',
+    }),
+    esbuild.build({
+      ...baseOptions,
+      outfile: 'dist/index.cjs',
+      format: 'cjs',
+    }),
+  ]);
+}
+
+build();
